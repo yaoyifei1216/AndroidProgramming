c3348f2871c78b73c1db36f6663a32bc0556634d
yaoyifei1216 <yyifei516@gmail.com>
2020-11-24 00:02:17 +0800

    use SQLite 'CRUD'

--- KotlinStarted/app/src/main/java/com/example/kotlinstarted/storagepersistencertest/FilePersistenceActivity.kt
+++ KotlinStarted/app/src/main/java/com/example/kotlinstarted/storagepersistencertest/FilePersistenceActivity.kt
@@ -1,14 +1,13 @@
 package com.example.kotlinstarted.storagepersistencertest
 
+import android.content.ContentValues
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlinstarted.R
 import kotlinx.android.synthetic.main.activity_file_persistence.*
 import java.io.*
-import java.lang.StringBuilder
 
 class FilePersistenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +31,46 @@ class FilePersistenceActivity : AppCompatActivity() {
             Toast.makeText(this, "$name", Toast.LENGTH_SHORT).show()
         }
         val dbHelper = MyDatabaseHelper(this, "BookStore.db", 1)//创建数据库
+        val db = dbHelper.writableDatabase //创建的数据库
         createDatabase.setOnClickListener {
             dbHelper.writableDatabase
+        }
+        addData.setOnClickListener {
+            val values1 = ContentValues().apply {
+                put("name", "java")
+                put("author", "feige")
+                put("price", 12.9)
+                put("pages", 100)
+            }
+            db.insert("Book", null, values1)
+            val values2 = ContentValues().apply {
+                put("name", "android")
+                put("author", "yaoyifei")
+                put("price", 13.8)
+                put("pages", 200)
+            }
+            db.insert("Book", null, values2)
+        }
+        updateData.setOnClickListener {
+            val values = ContentValues()
+            values.put("price", 9.9)
+            db.update("Book", values, "name = ?", arrayOf("java"))
+        }
+        deleteData.setOnClickListener {
+            db.delete("Book", "pages > ?", arrayOf("150"))
+        }
+        queryData.setOnClickListener {
+            val cursor = db.query("Book", null, null, null, null, null, null)
+            if (cursor.moveToFirst()) {
+                do {
+                    val name = cursor.getString(cursor.getColumnIndex("name"))
+                    val author = cursor.getString(cursor.getColumnIndex("author"))
+                    val pages = cursor.getInt(cursor.getColumnIndex("pages"))
+                    val price = cursor.getDouble(cursor.getColumnIndex("price"))
+                    editText.setText("name is:$name,author is:$author,pages is:$pages,price is:$price")
+                } while (cursor.moveToNext())
+            }
+            cursor.close()
         }
     }
 
